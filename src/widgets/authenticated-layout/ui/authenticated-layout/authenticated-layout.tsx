75ba46503f18667b8814fd06ebe918775a1d860e
A.A. Ivanov <artem.ivanov@indels.org>
2025-11-19 17:10:10 +0900

    fix: update register-city-page

--- src/widgets/authenticated-layout/ui/authenticated-layout/authenticated-layout.tsx
+++ src/widgets/authenticated-layout/ui/authenticated-layout/authenticated-layout.tsx
@@ -1,12 +1,13 @@
-import { useAuthState } from '@/entities/auth'
 import { Navigate, Outlet } from '@tanstack/react-router'
+
+import { useAuthState } from '@/entities/auth'
 
 export const AuthenticatedLayout = () => {
     const { isAuthenticated } = useAuthState()
-    
-    // if (!isAuthenticated) {
-    //     return <Navigate to='/login' />
-    // }
+
+    if (!isAuthenticated) {
+        return <Navigate to='/login' />
+    }
 
     return <Outlet />
 }
