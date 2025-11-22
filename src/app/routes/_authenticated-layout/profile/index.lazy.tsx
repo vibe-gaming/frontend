79a03769422dafc4f7fa4b62eed3dc506099d4d7
UiguunaMikhailova <uyguuna1997@gmail.com>
2025-11-22 14:01:44 +0900

    fix: try fix pwa 304 error

--- src/app/routes/_authenticated-layout/profile/index.lazy.tsx
+++
@@ -1,7 +0,0 @@
-import { createLazyFileRoute } from '@tanstack/react-router'
-
-import { ProfilePage } from '@/pages/profile-page'
-
-export const Route = createLazyFileRoute('/_authenticated-layout/profile/')({
-    component: ProfilePage,
-})
--- src/app/routes/_authenticated-layout/profile/index.tsx
+++ src/app/routes/_authenticated-layout/profile/index.tsx
@@ -1,6 +1,9 @@
 import { createFileRoute } from '@tanstack/react-router'
 
+import { ProfilePage } from '@/pages/profile-page'
+
 export const Route = createFileRoute('/_authenticated-layout/profile/')({
+    component: ProfilePage,
     onEnter: () => {
         // document.title = 'Профиль'
     },
