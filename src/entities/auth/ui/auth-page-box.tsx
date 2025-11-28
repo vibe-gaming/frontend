a8ee51a4e7991fe8226cf4053e642adf3f1f375b
A.A. Ivanov <artem.ivanov@indels.org>
2025-11-28 14:47:29 +0900

    feat: update auth

--- src/entities/auth/ui/auth-page-box.tsx
+++ src/entities/auth/ui/auth-page-box.tsx
@@ -11,7 +11,7 @@ export const AuthPageBox: React.FC<React.PropsWithChildren> = ({ children }) => 
             display='flex'
             flexDirection='column'
             maxW='1280px'
-            minH={{ base: '100dvh', md: 'calc(100dvh - 144px)' }}
+            minH={{ base: 'calc(100dvh - 80px)', md: 'calc(100dvh - 144px)' }}
             mx='auto'
             px={{ base: '16px', md: '0' }}
             py={isDesktop ? '0' : '20px'}
